88e99df5bc7bde3a29e0d4a7ed614f653841195a
ville24 <73233385+ville24@users.noreply.github.com>
2020-11-20 15:41:41 +0200

    osa 3.8

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+
+
+morgan.token('body', function (request, response) { return JSON.stringify(request.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json()) 
 
