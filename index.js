7f284e357c0a9c42c580baa304129eabe675c2ee
ville24 <73233385+ville24@users.noreply.github.com>
2020-12-02 17:31:16 +0200

    osa 3.18

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const cors = require('cors')
 const app = express()
 const Person= require('./models/person')
 
+app.use(express.static('build'))
+app.use(express.json()) 
 
 morgan.token('body', function (request, response) { return JSON.stringify(request.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(express.json()) 
-
 app.use(cors())
-
-app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
   Person.find().then(persons => {
@@ -21,6 +19,18 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).
+  then(person => {
+      if (person) {
+          response.json(person)
+      } 
+      else {
+        response.status(404).end()
+      }
+  })
+  .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -41,12 +51,56 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response, next) => {
+  Person.find()
+    .then(persons => {
+      response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${(new Date())}</p>
+        `)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, '0.0.0.0', () => {
